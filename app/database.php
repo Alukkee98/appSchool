664d4c204bdec29bcab3b23e99c2d6246d04acc7
Alex Luque <alex.luque.prados>
2019-11-09 17:55:05 +0100

    Test

--- app/database.php
+++ app/database.php
@@ -7,7 +7,13 @@ $database = 'app';
 
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+  
 } catch (PDOException $e) {
   die('Connection Failed: ' . $e->getMessage());
+}
+
+$link = mysqli_connect($server, $username, $password, $database);
+if (!$link) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
--- app/register.php
+++ app/register.php
@@ -10,21 +10,21 @@
 
 if ( !empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['email']) ) {
 	    
-	$sql = "INSERT INTO login_user (username, password, group_user_id, email, name, lastname) VALUES (:username, :password, :groupUserId, :email, :firstName, :lastName)";
+	$sql = "INSERT INTO user (username, password, group_user_id, email, name, lastname) VALUES (:username, :password, :groupUserId, :email, :firstName, :lastName)";
 	$stmt = $conn->prepare($sql);
 	
 	$stmt->bindParam(':username', $_POST['username']);
-  $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-  $stmt->bindParam(':password', $password);
+	$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+	$stmt->bindParam(':password', $password);
 	$stmt->bindParam(':email', $_POST['email']);
 	$stmt->bindParam(':groupUserId', $_POST['groupUserId']);
 	$stmt->bindParam(':firstName', $_POST['firstName']);
 	$stmt->bindParam(':lastName', $_POST['lastName']);
 
-  $sqlVerify = "'SELECT COUNT(*) FROM login_user WHERE email = :email' ";
-  $records = $conn->prepare($sqlVerify);
-  $records->bindParam(':email', $_POST['email']);
-  $records->execute();
+	$sqlVerify = "'SELECT COUNT(*) FROM user WHERE email = :email' ";
+	$records = $conn->prepare($sqlVerify);
+	$records->bindParam(':email', $_POST['email']);
+	$records->execute();
 
 
   
@@ -108,12 +108,31 @@ if ( !empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['
 						  <option value="1">ADMINISTRATOR</option>
 						  <option value="2">COMMON</option>
 						</select>
+						
+						<select class="form-control mdb-select md-form" name="groupUserId">
+							<option value="" disabled selected>*Choose your permission</option>
+						<?php
+						/* Realizamos la consulta para extraer los datos
+													
+								if ($valores != )) {
+									$message = 'Successfully created new user' ;
+									} else {
+									$message = 'Sorry there must have been an issue creating your account';
+								}
+								/*  while ($valores = mysqli_fetch_array($query)) {
+								// En esta sección estamos llenando el select con datos extraidos de una base de datos.
+								   echo '<option value="'.$valores[id].'">'.$valores[descripcion].'</option>';
+								  }*/
+								  
+								$sql = "SELECT * FROM group_user";
+								$result = mysqli_query($link, $sql);
+								if($result->num_rows>0){
+										echo '<option value="'.$valores[id].'">'.$valores[descripcion].'</option>';
+								}
+								?>
+						</select>
 				  </div>
 				</div>
-				
-               <!-- <a href="login.php" class="btn btn-primary btn-user btn-block">
-                  Register Account
-                </a>-->
 				<input type="submit" class="btn btn-primary btn-user btn-block" id="register" value="Register Account">					
                 <!--<a href="index.html" class="btn btn-google btn-user btn-block">
                   <i class="fab fa-google fa-fw"></i> Register with Google
