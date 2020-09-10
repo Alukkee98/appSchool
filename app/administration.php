0329cf16e82507475098b3b3845b8748c0f23461
Alex Luque <alex.luque.prados>
2020-09-10 12:54:42 +0200

    Update-v2

--- app/administration.php
+++ app/administration.php
@@ -6,9 +6,8 @@
 
   if (isset($_SESSION['id_user'])) {
 	  //Cargar datos user
-	$_SESSION['id_user'];
-	
-	$id_user = $_SESSION['id_user'];
+  	$_SESSION['id_user'];
+  	$id_user = $_SESSION['id_user'];
   }else{
 	 header('Location: login.php');
   }							
